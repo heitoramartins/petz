4238d2de723bbf605401f9f221ca088e9b6319a0
heitoramartins <heitoramartins@gmail.com>
2020-07-27 12:46:44 -0300

    first commit

---
+++ src/main/java/com/petz/entities/Address.java
@@ -0,0 +1,30 @@
+package com.petz.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	@Column(name = "country")
+	private String country;
+
+	@Column(name = "city")
+	private String city;
+
+	public Address(Long id) {
+		this.id = id;
+	}
+}
---
+++ src/main/java/com/petz/entities/Pet.java
@@ -0,0 +1,29 @@
+package com.petz.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet  {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "age")
+	private Long age;
+
+	@ManyToOne
+	private Client client;
+
+
+}
