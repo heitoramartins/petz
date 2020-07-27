f1c9e1e95ebe0ab649cf91f931bac0f2ddb60105
hmartins <heitor.martins@leega.com>
2020-07-27 14:02:59 -0300

    ajustes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
+EXPOSE 8080
 COPY target/petz-0.0.1-SNAPSHOT.jar petz.jar
 ADD target/petz-0.0.1-SNAPSHOT.jar petz.jar
 ENTRYPOINT ["java","-jar","/petz.jar"]
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -43,7 +43,7 @@ CREATE TABLE `client` (
   PRIMARY KEY (`id`),
   KEY `FKl63rkjkd4rggxh14d9ydtei43` (`address_id`),
   CONSTRAINT `FKl63rkjkd4rggxh14d9ydtei43` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of client
